126926d041be97372a753a78941d414b576d1509
IanStacked <ian.rodgers156@gmail.com>
2025-09-10 11:45:39 -0700

    add find_len_string function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
+    
+def find_len_string(string):
+        return len(string.split())
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    text = get_book_text("books/frankenstein.txt")
+    len = find_len_string(text)
+    print(f"{len} words found in the document")
 
 main()
